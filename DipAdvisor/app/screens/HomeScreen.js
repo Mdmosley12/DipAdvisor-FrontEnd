9e086541b156926369577d94044d6a0438e5f6ac
Mdmosley12 <mdmosley12@gmail.com>
2023-02-22 08:44:42 +0000

    add user login via firebase

--- DipAdvisor/app/screens/HomeScreen.js
+++ DipAdvisor/app/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { auth } from '../assets/firebase';
 
 function HomeScreen(props) {
     return (
         <View style={styles.background}>
-            <Text>Home Screen</Text>
+            <Text>Welcome {auth.currentUser?.email}</Text>
         </View>
     );
 }
