c9a7502bb37350e7281335a3f3a377e66dc5dc98
bergamotBen <bergamotBen@icloud.com>
2023-02-22 15:28:02 +0000

    userContext implemented

--- DipAdvisor/App.js
+++ DipAdvisor/App.js
@@ -5,26 +5,41 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import LoginScreen from "./app/screens/LoginScreen";
 import { SafeAreaView, StyleSheet } from "react-native";
 import HomeScreen from "./app/screens/HomeScreen";
+import { UserProvider } from "./app/contexts/UserContext";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <UserProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="WelcomeScreen"
+              component={WelcomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="LoginScreen"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </UserProvider>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
--- DipAdvisor/app/screens/HomeScreen.js
+++ DipAdvisor/app/screens/HomeScreen.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { auth } from '../assets/firebase';
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { auth } from "../assets/firebase";
 
 function HomeScreen(props) {
-    return (
-        <View style={styles.background}>
-            <Text>Welcome {auth.currentUser?.email}</Text>
-        </View>
-    );
+  return (
+    <View style={styles.background}>
+      <Text>Welcome {auth.currentUser?.email}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default HomeScreen;
