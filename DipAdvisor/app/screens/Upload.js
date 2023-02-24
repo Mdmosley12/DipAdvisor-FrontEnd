762d3c787e51c211534407a2d4f99b9394dd6d48
bergamotBen <bergamotBen@icloud.com>
2023-02-24 14:14:14 +0000

    uploads return URL which is set in state on completion

--- DipAdvisor/app/screens/Upload.js
+++ DipAdvisor/app/screens/Upload.js
@@ -1,42 +1,31 @@
 import { SafeAreaView, Text, StyleSheet } from "react-native";
-import React, { useState, useEffect } from "react";
-import { Button, Image, View, Platform } from "react-native";
+import React, { useState } from "react";
+import { Button, Image, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { ref, uploadBytes, getStorage } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { storage } from "../assets/firebase";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
 import "firebase/storage";
 
 function ImagePickerExample() {
-  async function uploadImageAsync(uri) {
-    // Why are we using XMLHttpRequest? See:
-    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
-
-    // const blob = await new Promise((resolve, reject) => {
-    //   const xhr = new XMLHttpRequest();
-    //   xhr.onload = function () {
-    //     resolve(xhr.response);
-    //   };
-    //   xhr.onerror = function (e) {
-    //     console.log(e);
-    //     reject(new TypeError("Network request failed"));
-    //   };
-    //   xhr.responseType = "blob";
-    //   xhr.open("GET", uri, true);
-    //   xhr.send(null);
-    // });
-
-    const fileRef = ref(getStorage(), uuid.v4());
-    const result = await uploadBytes(fileRef, blob);
-
-    // We're done with the blob, close and release it
-    blob.close();
-
-    return await getDownloadURL(fileRef);
-  }
-
   const [image, setImage] = useState(null);
+  const [imageURL, setImageURL] = useState("");
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   const uploadImage = async () => {
     const storageRef = ref(storage, uuidv4());
@@ -54,30 +43,20 @@ function ImagePickerExample() {
       xhr.open("GET", image, true);
       xhr.send(null);
     });
-    // 'file' comes from the Blob or File API
-    uploadBytes(storageRef, blob).then((snapshot) => {
-      console.log(snapshot);
-      console.log("Uploaded a blob or file!");
-      snapshot.ref.getDownloadURL().then(function (downloadURL) {
-        console.log("File available at", downloadURL);
+
+    // 'blob' comes from the Blob
+    uploadBytes(storageRef, blob)
+      .then((snapshot) => {
+        console.log(snapshot);
+        console.log("Uploaded a blob!");
+        return snapshot;
+      })
+      .then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          setImageURL(url);
+          console.log("File available at", url);
+        });
       });
-    });
-  };
-
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
   };
 
   return (
