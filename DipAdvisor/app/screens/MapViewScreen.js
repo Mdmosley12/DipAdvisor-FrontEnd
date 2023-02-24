09a61f7349814838a74327370496fbfd276a04a5
Wiggy93 <110779323+Wiggy93@users.noreply.github.com>
2023-02-24 17:12:55 +0000

    map showing, user location added, markers mapped, working on callout feature to show pics, need to add link to single location page

--- DipAdvisor/app/screens/MapViewScreen.js
+++ DipAdvisor/app/screens/MapViewScreen.js
@@ -3,63 +3,66 @@ import {
   Text,
   StyleSheet,
   SafeAreaView,
+  Image,
   TouchableOpacity,
 } from "react-native";
 
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Callout } from "react-native-maps";
+import * as Location from "expo-location";
 import { useState, useEffect } from "react";
 import { getAllLocations } from "../utils/api";
 
 function MapViewScreen({ navigation }) {
-  // const [startMapRegion, setStartMapRegion] = useState({
-  //   latitude: 53.456215,
-  //   longitude: -1.765994,
-  //   latitudeDelta: 0.0922,
-  //   longitudeDelta: 0.0421,
-  // });
   const [isLoading, setIsLoading] = useState(false);
   const [mapMarkers, setMapMarkers] = useState([]);
   const [userLocation, setUserLocation] = useState(null);
-  //need useEffect to get current location
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [selectedMarker, setSelectedMarker] = useState(null);
 
-  //get user location at start
-  // useEffect(() => {
-  //   (async () => {
-  //     let { status } = await Location.requestForegroundPermissionsAsync();
-  //     if (status !== "granted") {
-  //       Alert.alert("Permission to access location was denied");
-  //       return;
-  //     }
+  // gets user location at start, asks for permission if not already granted
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
 
-  //     let location = await Location.getCurrentPositionAsync({});
-  //     setUserLocation(location);
-  //   })();
-  // }, []);
+      let location = await Location.getCurrentPositionAsync({});
+      setUserLocation(location);
+    })();
+  }, []);
 
   useEffect(() => {
     setIsLoading(true);
-    //get use location on rendering of the page
-    //setStartMapRegion as an outcome
-    // GET all coordinates and location name in API.
     getAllLocations().then((data) => {
       const allCoordinates = data.map((location) => {
         return {
+          id: location._id,
           location_name: location.location_name,
           coordinate: {
             latitude: location.coordinates[0],
             longitude: location.coordinates[1],
           },
+          images: location.image_urls[0],
         };
       });
-      console.log(allCoordinates);
       setMapMarkers(allCoordinates);
-      // map these into coordinate and title list with setMapMarkers. Each one has a route set up for /api/locations/:id where id = _id value
+      //Each one has a route set up for /api/locations/:id where id = _id value
       setIsLoading(false);
     });
   }, []);
 
-  //potentially onChange of viewable map area, rerender shown markers, this may be inherrent part of maps.
+  console.log(mapMarkers, "<<<map markers");
+  console.log(userLocation, "<<<user location");
 
+  const handleMarkerPress = (marker) => {
+    setSelectedMarker(marker);
+  };
+
+  const markerClick = () => {
+    console.log("callout clicked");
+  };
   //functions to use:
   //onRegionChange callback function
   //onMarkerSelect
@@ -69,30 +72,51 @@ function MapViewScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Find your nearest outdoor swimming location</Text>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 52,
-          longitude: -1,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        {/* // showsUserLocation={true} requires permissions, see react-native-permissions
-      // showsMyLocationButton={true} moves to users current location
-      //zoomEnabled={true}
-    //showsScale={true} */}
 
-        {mapMarkers.map((marker, index) => {
-          return (
-            <Marker
-              key={index}
-              coordinate={marker.coordinate}
-              title={marker.location_name}
-            />
-          );
-        })}
-      </MapView>
+      {userLocation && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: userLocation.coords.latitude,
+            longitude: userLocation.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          showsScale={true}
+          zoomEnabled={true}
+          onCalloutPress={markerClick}
+        >
+          {mapMarkers.map((marker, index) => {
+            return (
+              <Marker
+                key={index}
+                coordinate={marker.coordinate}
+                title={marker.location_name + "666"}
+                onPress={() => handleMarkerPress(marker)}
+              >
+                {selectedMarker === marker && (
+                  <Callout>
+                    <View style={styles.calloutContainer}>
+                      {/* //Text not showing */}
+                      <Text style={styles.calloutTitle}>
+                        {/* //42 put in to see which title is appearing */}
+                        {marker.location_name + "42"}
+                      </Text>
+                      {/* //Image not showing */}
+                      <Image
+                        source={{ uri: marker.images }}
+                        style={styles.calloutImage}
+                      />
+                    </View>
+                  </Callout>
+                )}
+              </Marker>
+            );
+          })}
+        </MapView>
+      )}
+      {errorMsg && <Text>{errorMsg}</Text>}
     </SafeAreaView>
   );
 }
@@ -100,13 +124,29 @@ function MapViewScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
+    height: "100%",
+    width: "100%",
     justifyContent: "flex-end",
     alignItems: "center",
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  calloutContainer: {
+    width: 160,
+    height: 200,
+    alignItems: "center",
+  },
+  calloutTitle: {
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  calloutImage: {
+    width: "100%",
+    height: "80%",
+    resizeMode: "cover",
   },
 });
 
