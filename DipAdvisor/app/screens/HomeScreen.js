c1c74c471254e021abb75ce8dd910b44e2fe8107
bergamotBen <bergamotBen@icloud.com>
2023-02-27 11:13:54 +0000

    navigation passed down to component through props

--- DipAdvisor/app/screens/HomeScreen.js
+++ DipAdvisor/app/screens/HomeScreen.js
@@ -3,61 +3,56 @@ import { UserContext } from "../contexts/UserContext";
 import { useContext } from "react";
 import { auth } from "../assets/firebase";
 import { getTopLocations } from "../utils/api";
-import { Ionicons } from "@expo/vector-icons";
-import { Formik } from "formik";
 import { styles } from "../styles/styles.HomeScreen";
 
-import {
-  Keyboard,
-  Text,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
-
-const PopularSpotsBox = ({ popularSpots }) => {
+import { Text, Image, TouchableOpacity, View } from "react-native";
+const PopularSpotsBox = ({ popularSpots, navigation }) => {
   return (
     <View>
       <Text>Most popular spots:</Text>
       {popularSpots.map((spot) => {
-        return <PopularSpotBox spot={spot} key={spot._id} />;
+        return (
+          <PopularSpotBox spot={spot} key={spot._id} navigation={navigation} />
+        );
       })}
     </View>
   );
 };
 
-const PopularSpotBox = ({ spot }) => {
+const PopularSpotBox = ({ spot, navigation }) => {
+  const goToLocation = (locationID) => {
+    navigation.navigate("SingleLocationScreen", locationID);
+  };
   return (
     <View>
-      <Text>{spot.location_name}</Text>
-      <Image
-        style={{ width: 50, height: 50 }}
-        source={{ uri: spot.image_urls[0] }}
-      />
-      <Text> Votes: {spot.votes}</Text>
+      <TouchableOpacity onPress={() => goToLocation(spot._id)}>
+        <Text>{spot.location_name}</Text>
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={{ uri: spot.image_urls[0] }}
+        />
+        <Text> Votes: {spot.votes}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
-
-const HomeScreen = ({ navigation }, props) => {
-  const userValue = useContext(UserContext);
+const HomeScreen = ({ navigation }) => {
   const [popularSpots, setPopularSpots] = useState([]);
+
   useEffect(() => {
     getTopLocations().then((data) => {
       setPopularSpots(data);
     });
   }, []);
 
-  const handleGetLocation = (values) => {
-    navigation.push("SingleLocationScreen", values);
-  };
-
+  // const handleGetLocation = (values) => {
+  //   navigation.push("SingleLocationScreen", values);
+  // };
   return (
     <View style={styles.container}>
       <View style={styles.backgroundWelcome}>
         <Text>Where's today's dip {auth.currentUser.displayName}?</Text>
-        <PopularSpotsBox popularSpots={popularSpots} />
+        <PopularSpotsBox popularSpots={popularSpots} navigation={navigation} />
       </View>
     </View>
   );
