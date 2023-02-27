d4a7f4326f05b72779d1789fe3aeb28319817457
lewqs <lewisbrown3108@gmail.com>
2023-02-27 12:05:42 +0000

    Added marginTopChanger to utils and implememented it into login/signup screens

--- DipAdvisor/app/screens/LoginScreen.js
+++ DipAdvisor/app/screens/LoginScreen.js
@@ -1,21 +1,17 @@
-import React, { useEffect } from "react";
-import {
-  View,
-  Text,
-  ImageBackground,
-  TextInput,
-  Button,
-  KeyboardAvoidingView,
-} from "react-native";
 import { Formik } from "formik";
+import React, { useContext, useEffect, useState } from "react";
+import { Button, ImageBackground, Text, TextInput, View } from "react-native";
 import { CheckBox } from "react-native-elements";
 import { auth } from "../assets/firebase";
 import { UserContext } from "../contexts/UserContext";
-import { useContext } from "react";
 import { styles } from "../styles/styles.LoginScreen";
+import { marginTopChanger } from "../utils/marginTopChanger";
 
 function LoginScreen({ navigation }) {
   const userValue = useContext(UserContext);
+  const [containerMarginTop, setContainerMarginTop] = useState(125);
+
+  marginTopChanger(setContainerMarginTop);
 
   const handleLogin = (values) => {
     if (values.isChecked === true) {
@@ -42,59 +38,62 @@ function LoginScreen({ navigation }) {
   return (
     <ImageBackground
       style={styles.background}
-      source={require("../assets/WelcomeScreenImg.jpg")}
-    >
-      <KeyboardAvoidingView behavior="padding">
-        <Formik
-          initialValues={{ email: "", password: "", isChecked: false }}
-          onSubmit={(values) => {
-            handleLogin(values);
-          }}
-        >
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            values,
-            setFieldValue,
-          }) => (
-            <View style={styles.loginContainer}>
-              <Text style={styles.label}>Email:</Text>
-              <TextInput
-                style={styles.emailInput}
-                onChangeText={handleChange("email")}
-                onBlur={handleBlur("email")}
-                value={values.email}
-              />
-              <Text style={styles.label}>Password:</Text>
-              <TextInput
-                style={styles.passwordInput}
-                secureTextEntry
-                onChangeText={handleChange("password")}
-                onBlur={handleBlur("password")}
-                value={values.password}
-              />
-              <Text style={styles.disclaimer}>
-                By using this App, the user agrees to indemnify and hold
-                harmless the App and its developers, affiliates, and partners
-                from any claims, damages, or expenses arising out of or in
-                connection with the use of the information provided.
-              </Text>
-              <CheckBox
-                style={styles.checkbox}
-                title="Accept terms & conditions"
-                checked={values.isChecked}
-                onPress={() => setFieldValue("isChecked", !values.isChecked)}
-              />
-              <Button
-                style={styles.button}
-                onPress={handleSubmit}
-                title="Log In"
-              />
-            </View>
-          )}
-        </Formik>
-      </KeyboardAvoidingView>
+      source={require("../assets/WelcomeScreenImg.jpg")}>
+      <Formik
+        initialValues={{ email: "", password: "", isChecked: false }}
+        onSubmit={(values) => {
+          handleLogin(values);
+        }}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          setFieldValue,
+        }) => (
+          <View
+            style={{
+              ...styles.loginContainer,
+              marginTop: containerMarginTop,
+            }}>
+            <Text style={styles.label}>Email:</Text>
+            <TextInput
+              style={styles.emailInput}
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+              autoCapitalize="none"
+              keyboardType="email-address"
+            />
+            <Text style={styles.label}>Password:</Text>
+            <TextInput
+              style={styles.passwordInput}
+              secureTextEntry
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+              autoCapitalize="none"
+            />
+            <Text style={styles.disclaimer}>
+              By using this App, the user agrees to indemnify and hold harmless
+              the App and its developers, affiliates, and partners from any
+              claims, damages, or expenses arising out of or in connection with
+              the use of the information provided.
+            </Text>
+            <CheckBox
+              style={styles.checkbox}
+              title="Accept terms & conditions"
+              checked={values.isChecked}
+              onPress={() => setFieldValue("isChecked", !values.isChecked)}
+            />
+            <Button
+              style={styles.button}
+              onPress={handleSubmit}
+              title="Log In"
+            />
+          </View>
+        )}
+      </Formik>
     </ImageBackground>
   );
 }
--- DipAdvisor/app/screens/SignUpScreen.js
+++ DipAdvisor/app/screens/SignUpScreen.js
@@ -1,18 +1,15 @@
 import { Formik } from "formik";
-import React, { useEffect } from "react";
-import {
-  ImageBackground,
-  KeyboardAvoidingView,
-  View,
-  Text,
-  TextInput,
-  Button,
-} from "react-native";
-import { auth } from "../assets/firebase";
+import React, { useEffect, useState } from "react";
+import { Button, ImageBackground, Text, TextInput, View } from "react-native";
 import { CheckBox } from "react-native-elements";
+import { auth } from "../assets/firebase";
 import { styles } from "../styles/styles.SignUpScreen";
+import { marginTopChanger } from "../utils/marginTopChanger";
 
 function SignUpScreen({ navigation }) {
+  const [containerMarginTop, setContainerMarginTop] = useState(125);
+  marginTopChanger(setContainerMarginTop);
+
   const handleSignUp = (values) => {
     if (values.isChecked === true) {
       auth
@@ -40,71 +37,76 @@ function SignUpScreen({ navigation }) {
   return (
     <ImageBackground
       style={styles.background}
-      source={require("../assets/WelcomeScreenImg.jpg")}
-    >
-      <KeyboardAvoidingView behavior="padding">
-        <Formik
-          initialValues={{
-            email: "",
-            displayName: "",
-            password: "",
-            isChecked: false,
-          }}
-          onSubmit={(values) => {
-            handleSignUp(values);
-          }}
-        >
-          {({
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            values,
-            setFieldValue,
-          }) => (
-            <View style={styles.loginContainer}>
-              <Text style={styles.label}>Email:</Text>
-              <TextInput
-                style={styles.emailInput}
-                onChangeText={handleChange("email")}
-                onBlur={handleBlur("email")}
-                value={values.email}
-              />
-              <Text style={styles.label}>Display Name:</Text>
-              <TextInput
-                style={styles.displayNameInput}
-                onChangeText={handleChange("displayName")}
-                onBlur={handleBlur("displayName")}
-                value={values.displayName}
-              />
-              <Text style={styles.label}>Password:</Text>
-              <TextInput
-                style={styles.passwordInput}
-                secureTextEntry
-                onChangeText={handleChange("password")}
-                onBlur={handleBlur("password")}
-                value={values.password}
-              />
-              <Text style={styles.disclaimer}>
-                By using this App, the user agrees to indemnify and hold
-                harmless the App and its developers, affiliates, and partners
-                from any claims, damages, or expenses arising out of or in
-                connection with the use of the information provided.
-              </Text>
+      source={require("../assets/WelcomeScreenImg.jpg")}>
+      <Formik
+        initialValues={{
+          email: "",
+          displayName: "",
+          password: "",
+          isChecked: false,
+        }}
+        onSubmit={(values) => {
+          handleSignUp(values);
+        }}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          setFieldValue,
+        }) => (
+          <View
+            style={{
+              ...styles.loginContainer,
+              marginTop: containerMarginTop,
+            }}>
+            <Text style={styles.label}>Email:</Text>
+            <TextInput
+              style={styles.emailInput}
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+              autoCapitalize="none"
+              keyboardType="email-address"
+            />
+            <Text style={styles.label}>Display Name:</Text>
+            <TextInput
+              style={styles.displayNameInput}
+              onChangeText={handleChange("displayName")}
+              onBlur={handleBlur("displayName")}
+              value={values.displayName}
+            />
+            <Text style={styles.label}>Password:</Text>
+            <TextInput
+              style={styles.passwordInput}
+              secureTextEntry
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+              autoCapitalize="none"
+            />
+            <Text style={styles.disclaimer}>
+              By using this App, the user agrees to indemnify and hold harmless
+              the App and its developers, affiliates, and partners from any
+              claims, damages, or expenses arising out of or in connection with
+              the use of the information provided.
+            </Text>
+            <View style={styles.checkboxContainer}>
               <CheckBox
                 style={styles.checkbox}
                 title="Accept terms & conditions"
                 checked={values.isChecked}
                 onPress={() => setFieldValue("isChecked", !values.isChecked)}
               />
-              <Button
-                style={styles.button}
-                onPress={handleSubmit}
-                title="Sign Up"
-              />
             </View>
-          )}
-        </Formik>
-      </KeyboardAvoidingView>
+            <Button
+              style={styles.button}
+              onPress={handleSubmit}
+              title="Sign Up"
+            />
+          </View>
+        )}
+      </Formik>
     </ImageBackground>
   );
 }
