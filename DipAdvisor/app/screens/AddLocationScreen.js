f35ac6d40a0960a203a71e5f5444d9d006c779e5
bergamotBen <bergamotBen@icloud.com>
2023-02-27 13:47:53 +0000

    fixing uploads

--- DipAdvisor/app/screens/AddLocationScreen.js
+++ DipAdvisor/app/screens/AddLocationScreen.js
@@ -16,7 +16,8 @@ import { addLocation } from "../utils/api";
 import { styles } from "../styles/styles.AddLocationScreen";
 import { useState } from "react";
 import "react-native-get-random-values";
-import { uploadImage, pickImage } from "../utils/imageUploads";
+import { uploadImage } from "../utils/imageUploads";
+import * as ImagePicker from "expo-image-picker";
 
 function AddLocationScreen({ navigation }) {
   const [image, setImage] = useState(null);
@@ -31,7 +32,18 @@ function AddLocationScreen({ navigation }) {
       });
     });
   };
+  const pickImage = async (setImage) => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
     <ImageBackground
       style={styles.background}
@@ -71,6 +83,16 @@ function AddLocationScreen({ navigation }) {
                   value={values.description}
                 />
                 <Text style={styles.label}>
+                  <Button
+                    title="Select photo"
+                    onPress={() => pickImage(setImage)}
+                  />
+                  {image && (
+                    <Image
+                      source={{ uri: image }}
+                      style={{ width: 200, height: 200 }}
+                    />
+                  )}
                   Is this location on public land?
                 </Text>
                 <View style={styles.switchContainer}>
