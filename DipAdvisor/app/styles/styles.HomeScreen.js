05f6f8992b9aa38ab45b67230b4b7773e0dcd564
Mdmosley12 <mdmosley12@gmail.com>
2023-03-02 10:09:38 +0000

    add changes

--- DipAdvisor/app/styles/styles.HomeScreen.js
+++ DipAdvisor/app/styles/styles.HomeScreen.js
@@ -19,6 +19,19 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
+    shadowColor: "#000",
+    backgroundColor: "rgba(250, 250, 250, 0.8)",
+    borderRadius: 8,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+    marginRight: 10,
+    marginLeft: 10,
   },
   votes: {
     fontSize: 12,
@@ -33,8 +46,9 @@ const styles = StyleSheet.create({
   boxTitle: {
     fontSize: 16,
     textAlign: "center",
-    backgroundColor: "rgba(250, 250, 250, 0.7)",
+    // backgroundColor: "rgba(250, 250, 250, 0.7)",
     borderRadius: 10,
+    fontWeight: "bold",
   },
   backgroundWelcome: {
     flex: 1,
